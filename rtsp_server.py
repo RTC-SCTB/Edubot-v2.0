79d4e30be69bfdbb2c26e98010df7d3d1d1e3f1e
uiopio <lesha.tort2@mail.ru>
2020-12-22 12:26:15 +0300

    Сервер для трансляции видео с робота

--- rtsp_server.py
+++ rtsp_server.py
@@ -2,6 +2,9 @@
 # -*- coding:utf-8 vi:ts=4:noexpandtab
 # Simple RTSP server. Run as-is or with a command-line to replace the default pipeline
 
+""" Для получения доступа к трансляции необходимо ввести ссылку "rtsp://..." (выводится при запуске сервера) в проигрыватель rtsp потоков, например плеер vlc"""
+
+
 import sys
 import gi
 import os
@@ -14,25 +17,24 @@ loop = GLib.MainLoop()
 Gst.init(None)
 
 
-#Возвращает ip
+# Функция для получения IP робота
 def getIP():
-        #cmd = 'hostname -I | cut -d\' \' -f1'
-        #ip = subprocess.check_output(cmd, shell = True) #получаем IP
         res = os.popen('hostname -I | cut -d\' \' -f1').readline().replace('\n','') #получаем IP, удаляем \n
         return res
 
+# функция для формирования пайплайна
 class CamFactory(GstRtspServer.RTSPMediaFactory):
         def __init__(self):
                 GstRtspServer.RTSPMediaFactory.__init__(self)
 
         def do_create_element(self, url):
-                pipeline_str = "( v4l2src device=/dev/video0 ! video/x-raw, width=320, height=240 ! v4l2h264enc ! rtph264pay name=pay0 pt=96 )"
+                pipeline_str = "( v4l2src device=/dev/video0 ! video/x-raw, width=640, height=480 ! v4l2h264enc ! rtph264pay name=pay0 pt=96 )"
                 print(pipeline_str)
                 return Gst.parse_launch(pipeline_str)
 
 
 
-#Порт: 5554
+# класс сервера
 class Server():
 	def __init__(self):
 		self.Server = GstRtspServer.RTSPServer.new()
@@ -47,9 +49,9 @@ class Server():
 		self.Server.attach(None)
 
 		portServer = self.Server.get_bound_port()
-		print('RTSP server started: rtsp://%s:%d/front' % (getIP(), portServer))
+		print('RTSP server started: rtsp://%s:%d/front' % (getIP(), portServer)) # Вывод ссылки для подключения к серверу
 
 		
 if __name__ == '__main__':
-	server = Server()
+	server = Server() # инициализация сервера
 	loop.run()
